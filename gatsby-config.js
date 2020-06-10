751102b41de6409156dabfc39c7aa6afa44b23a1
Seb Toombs <sebtoombs@gmail.com>
2020-06-11 08:40:50 +1000

    Updated manifest config in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,11 +28,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `pattex-distillery`,
+        short_name: `pattex-distillery`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#ba0c2f`,
         display: `minimal-ui`,
         //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
