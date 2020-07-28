32319e46d4e56dc550cba78dd54993471a11f81f
Seb Toombs <sebtoombs@gmail.com>
2020-07-28 10:21:23 +1000

    Add og image

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,15 +10,24 @@ const path = require("path")
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
 
+  const ogImageTemplate = path.resolve(`src/templates/index.og-image.js`)
+  const size = { width: 1200, height: 630 }
   const openGraphImage = createOpenGraphImage(createPage, {
     path: "/og-image/index.png",
-    component: path.resolve(`src/templates/index.og-image.js`),
-    // size: {
-    //   width: 400,
-    //   height: 50,
-    // },
+    component: ogImageTemplate,
+    size,
     context: {
       description: "a image created with gatsby-plugin-open-graph-images",
     },
   })
+
+  if (process.env.NODE_ENV === `development`) {
+    createPage({
+      path: `/og-image/index`,
+      component: ogImageTemplate,
+      context: {
+        size,
+      },
+    })
+  }
 }
